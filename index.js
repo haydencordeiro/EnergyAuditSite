245912162e337e356ea7a9c0299b117d720f3f79
Hayden Corderio <cordeirohayden@gmail.com>
2021-05-27 19:23:29 +0530

    solved existing light table

--- index.js
+++ index.js
@@ -17,22 +17,22 @@ function onChangeEquipmentDropDown(){
     const ratedCap=document.getElementById("RatedCap");
     lumnens.value=specData[equipDropRef.value]["lumen"];
     ratedCap.value=specData[equipDropRef.value]["power"];
-    // var i=equipDropRef.value;
-    // document.getElementById("rec-table-body1").innerHTML=`
-    // <tr>
+    var i=equipDropRef.value;
+    document.getElementById("rec-table-body1").innerHTML=`
+    <tr>
     
-    // <td>${specData[i]["name"]}</td>
-    // <td>${specData[i]["power"]}</td>
-    // <td>${specData[i]["lumen"]}</td>
-    // <td>${specData[i]["System efficacy"]}</td>
-    // <td>${specData[i]["Color Temperature"]}</td>
-    // <td>${specData[i]["CRI"]}</td>
-    // <td>${specData[i]["Voltage (V)"]}</td>
-    // <td>${specData[i]["Life"]}</td>
-    // <td>${specData[i]["Application"]}</td>
+    <td>${specData[i]["name"]}</td>
+    <td>${specData[i]["power"]}</td>
+    <td>${specData[i]["lumen"]}</td>
+    <td>${specData[i]["System efficacy"]}</td>
+    <td>${specData[i]["Color Temperature"]}</td>
+    <td>${specData[i]["CRI"]}</td>
+    <td>${specData[i]["Voltage (V)"]}</td>
+    <td>${specData[i]["Life"]}</td>
+    <td>${specData[i]["Application"]}</td>
   
     
-    // </tr>`
+     </tr>`
     mainCalculation();
 
 }
